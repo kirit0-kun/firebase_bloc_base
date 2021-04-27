58b46cad0671236e929df01ebb2529397091a141
Moustafa Essam <drsh_201122@yahoo.com>
2021-04-27 18:06:30 +0200

    initial

---
+++ lib/src/presentation/bloc/base_provider/base_provider_dependant_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:firebase_bloc_base/firebase_bloc_base.dart';
+import 'package:firebase_bloc_base/src/presentation/bloc/base_provider/lifecycle_observer.dart';
+
+import 'base_provider_bloc.dart';
+
+abstract class BaseDependantProvider<Input, Output>
+    extends BaseProviderBloc<Input, Output> {
+  final Stream source;
+  StreamSubscription _subscription;
+
+  BaseDependantProvider(this.source, LifecycleObserver observer)
+      : super(getOnCreate: false, observer: observer) {
+    _subscription = source.listen((event) {
+      getData();
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
+  }
+}
